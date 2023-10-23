df7e5bb6a93441541096bcf1551552ccb93f55db
allfitra <alfitrafadjri00@gmail.com>
2023-10-23 19:56:28 +0700

    menyesuaikan image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/images/Logo1";
+import Logo from "../assets/images/Logo1.png";
 
 function Header() {
   const [isToggleOpen, setIsToggleOpen] = useState(false);
