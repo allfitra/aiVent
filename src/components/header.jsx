2f8212379dca54171df55e93e95cff6a88e23a58
allfitra <alfitrafadjri00@gmail.com>
2023-10-28 12:32:29 +0700

    add button login admin

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,9 +13,13 @@ function Header() {
             <img src={HLogo} className="ml-5 h-10" alt="aiVents Logo" />
           </Link>
           <div className="flex items-center lg:order-2">
-            {isLoggedIn === "true" && (
+            {isLoggedIn === "true" ? (
               <Link reloadDocument to={"/admin"} className="text-black dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 ">
                 Logout
+              </Link>
+            ) : (
+              <Link reloadDocument to={"/admin"} className="text-black dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 ">
+                Login for admin
               </Link>
             )}
 
